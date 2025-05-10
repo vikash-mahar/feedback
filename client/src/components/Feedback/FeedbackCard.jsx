4dfab94f34a0f0901422d2acbdcdd20f53170df2
vikash-mahar <vmahar808@gmail.com>
2025-05-10 19:34:10 +0530

    email added

--- client/src/components/Feedback/FeedbackCard.jsx
+++ client/src/components/Feedback/FeedbackCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Tweet({ tweet, page = false }) {
     return (<>
-  <figure className="relative rounded-2xl overflow-hidden max-w-xs pb-3 w-full text-left text-gray-800 bg-gray-300  shadow-md">
+  <figure className="relative rounded-2xl overflow-hidden max-w-xs pb-10 w-full text-left text-gray-800 bg-gray-300  shadow-md">
     <blockquote className=" bg-gray-100 p-6 text-sm italic leading-relaxed">
       {tweet?.content}
       <div className="absolute top-full left-0 w-0 h-0 border-l-transparent border-r-transparent border-l-[12.5px] border-r-[12.5px] border-t-[25px] border-t-gray-100"></div>
@@ -14,11 +14,12 @@ function Tweet({ tweet, page = false }) {
           className="w-[90px] h-[90px] flex items-end rounded-full mt-10 ml-2.5 object-cover"
         />
     </div>
-    <div className=" pl-[17px] pb-[10px]  text-white uppercase transform translate-y-1/2">
+    <div className=" pl-[17px] pb-[11px]  text-white uppercase transform translate-y-1/2">
 
       <h5 className="font-extrabold opacity-80 text-gray-950">
-        {tweet?.owner?.fullName}<span className="font-normal normal-case text-gray-800 pl-1"></span>
+        {tweet?.owner?.fullName}
       </h5>
+      <h5 className="font-normal normal-case text-gray-500 pl-1 ">{tweet?.owner?.email}</h5>
     </div>
   </figure>
 
