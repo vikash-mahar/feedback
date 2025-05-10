0f7eebe5af210f9db2111bfaa613e01f44efa428
vikash-mahar <vmahar808@gmail.com>
2025-05-10 19:33:49 +0530

    finish

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 const app=express()
 
 app.use(cors({
-    // origin:["http://localhost:5173",
+    // origin:["http://localhost:5173"],
     origin:["https://feedback-f65j.onrender.com"],
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
     credentials:true
